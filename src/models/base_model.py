33e84cc98ca030f834fa2077f915f709490eb0c0
okushnarev <styx.inc4@gmail.com>
2026-05-19 17:06:08 +0700

    Add lightning classification + regression model

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -84,3 +84,49 @@ class LitRegressionModel(LitBaseModel):
         # log with epoch
         metrics = {'epoch/reg/val_loss': loss.item()}
         self.process_epoch_metrics(metrics, batch_idx, self.trainer.num_val_batches[0])
+
+class LitMixedModel(LitBaseModel):
+    def training_step(self, batch, batch_idx):
+        X, y_cls, y_reg = batch
+        out_cls, out_reg = self(X)
+
+        # Classification
+        cls_loss = cross_entropy(out_cls, y_cls.squeeze())
+        self.log('cls/train_loss', cls_loss, prog_bar=True, on_epoch=True, on_step=False)
+        self.process_epoch_metrics({'epoch/cls/train_loss': cls_loss.item()}, batch_idx, self.trainer.num_training_batches)
+
+        # Regression
+        reg_loss = mse_loss(out_reg, y_reg.squeeze())
+        self.log('reg/train_loss', reg_loss, prog_bar=True, on_epoch=True, on_step=False)
+        self.process_epoch_metrics({'epoch/reg/train_loss': reg_loss.item()}, batch_idx, self.trainer.num_training_batches)
+
+        overall_loss = cls_loss + reg_loss
+        self.log('overall/train_loss', overall_loss, prog_bar=True, on_epoch=True, on_step=False)
+        return overall_loss
+
+    def validation_step(self, batch, batch_idx):
+        X, y_cls, y_reg = batch
+        out_cls, out_reg = self(X)
+
+        # Classification
+        cls_loss = cross_entropy(out_cls, y_cls.squeeze())
+        self.log('cls/val_loss', cls_loss, prog_bar=True, on_epoch=True, on_step=False)
+        # accuracy
+        predicted = torch.argmax(out_cls, 1)
+        correct = (predicted.view(-1, 1) == y_cls).sum().item()
+        accuracy = correct / len(y_cls)
+        self.log('cls/val_acc', accuracy, prog_bar=True, on_epoch=True, on_step=False)
+
+        metrics = {'epoch/cls/val_loss': cls_loss.item(), 'epoch/cls/val_acc': accuracy}
+        self.process_epoch_metrics(metrics, batch_idx, self.trainer.num_val_batches[0])
+
+        # Regression
+        reg_loss = mse_loss(out_reg, y_reg.squeeze())
+        self.log('reg/val_loss', reg_loss, prog_bar=True, on_epoch=True, on_step=False)
+        self.log('overall/val_loss', reg_loss, prog_bar=True, on_epoch=True, on_step=False)
+        # log with epoch
+        metrics = {'epoch/reg/val_loss': reg_loss.item()}
+        self.process_epoch_metrics(metrics, batch_idx, self.trainer.num_val_batches[0])
+
+        overall_loss = cls_loss + reg_loss
+        self.log('overall/val_loss', overall_loss, prog_bar=True, on_epoch=True, on_step=False)
