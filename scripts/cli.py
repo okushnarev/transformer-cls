a21f35c52c3ff4177b30f98f8f8c4b3b082bfe8b
okushnarev <styx.inc4@gmail.com>
2026-05-20 17:54:08 +0700

    Add automatic 'input_dim' calculation via CLI based on amount of datamodule's features

--- scripts/cli.py
+++ scripts/cli.py
@@ -10,6 +10,11 @@ sys.path.append(str(project_root))
 class MyLightningCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
         parser.link_arguments('data.init_args.sequence_length', 'model.init_args.sequence_length')
+        parser.link_arguments(
+            'data.init_args.features',
+            'model.init_args.input_dim',
+            compute_fn=len,
+        )
 
 def cli_main():
     cli = MyLightningCLI(parser_kwargs={'parser_mode': 'omegaconf'})
