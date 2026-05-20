c0ac54324c5779b0f9998a7b0105505abed0706b
okushnarev <styx.inc4@gmail.com>
2026-05-20 17:32:51 +0700

    Add CLI script for training with modularized project

---
+++ scripts/cli.py
@@ -0,0 +1,19 @@
+from lightning.pytorch.cli import LightningCLI
+
+import sys
+from pathlib import Path
+
+# Add project root to PATH
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_root))
+
+class MyLightningCLI(LightningCLI):
+    def add_arguments_to_parser(self, parser):
+        parser.link_arguments('data.init_args.sequence_length', 'model.init_args.sequence_length')
+
+def cli_main():
+    cli = MyLightningCLI(parser_kwargs={'parser_mode': 'omegaconf'})
+
+
+if __name__ == "__main__":
+    cli_main()
