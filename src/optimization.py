c1babd29f0c84f351e494738545a8cd055d5d3a4
okushnarev <styx.inc4@gmail.com>
2026-09-01 12:42:50 +0700

    Update objective, wrap with optimizer function

--- src/optimization.py
+++ src/optimization.py
@@ -1,3 +1,4 @@
+import inspect
 from typing import Callable, Any
 
 import optuna
@@ -5,26 +6,65 @@ import lightning as L
 from optuna.integration import PyTorchLightningPruningCallback
 
 
-def objective(
-        trial: optuna.trial.Trial,
+def optimize(
         model_class: L.LightningModule,
-        datamodule: L.LightningDataModule,
-        suggest_hparams: Callable[[], dict[str, Any]],
-        **trainer_kwargs
-) -> float:
-    hparams = suggest_hparams()
-    model = model_class(**hparams)
+        datamodule_class: L.LightningDataModule,
+        suggest_hparams: Callable[[optuna.trial.Trial], dict[str, Any]],
 
-    trainer = L.Trainer(
-        logger=True,
-        limit_val_batches=trainer_kwargs.get('limit_val_batches', 1),
-        enable_checkpointing=False,
-        max_epochs=trainer_kwargs.get('max_epochs', 20),
-        accelerator='auto',
-        devices=1,
-        callbacks=[PyTorchLightningPruningCallback(trial, monitor='overall/val_loss')],
+        n_trials=100,
+        timeout=600,
+
+        max_epochs: int = 30,
+        limit_val_batches: float = 1,
+        monitor_metric: str = 'overall/val_loss',
+) -> None:
+    def objective(
+            trial: optuna.trial.Trial,
+    ) -> float:
+        hparams = suggest_hparams(trial)
+        model = model_class(**hparams)
+
+        dm_sig = inspect.signature(datamodule_class)
+        dm_params = {}
+        for param, value in hparams.items():
+            if param in dm_sig.parameters:
+                dm_params[param] = value
+        dm = datamodule_class(**dm_params)
+
+        trainer = L.Trainer(
+            logger=True,
+            limit_val_batches=limit_val_batches,
+            enable_checkpointing=False,
+            max_epochs=max_epochs,
+            accelerator='auto',
+            devices=1,
+            callbacks=[PyTorchLightningPruningCallback(trial, monitor=monitor_metric)],
+        )
+        trainer.logger.log_hyperparams(hparams)
+        trainer.fit(model, datamodule=dm)
+
+        return trainer.callback_metrics[monitor_metric].item()
+
+    pruner = optuna.pruners.HyperbandPruner(
+        max_resource=max_epochs,
     )
-    trainer.logger.log_hyperparams(hparams)
-    trainer.fit(model, datamodule=datamodule)
 
-    return trainer.callback_metrics['overall/val_loss'].item()
+    study = optuna.create_study(direction='minimize', pruner=pruner)
+    study.optimize(
+        objective,
+        n_trials=n_trials,
+        timeout=timeout,
+        gc_after_trial=True,
+    )
+
+    print(f'Number of finished trials: {len(study.trials)}')
+
+    print('Best trial:')
+    trial = study.best_trial
+
+    print(f'  Value: {trial.value}')
+
+    print('  Params: ')
+    for key, value in trial.params.items():
+        print(f'    {key}: {value}')
+
