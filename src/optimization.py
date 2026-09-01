a94a356a6e80c2b8cb76621bc1a1ade9a5a68cf9
okushnarev <styx.inc4@gmail.com>
2026-09-01 12:46:03 +0700

    Add test code in `optimization.py`

--- src/optimization.py
+++ src/optimization.py
@@ -68,3 +68,60 @@ def optimize(
     for key, value in trial.params.items():
         print(f'    {key}: {value}')
 
+
+if __name__ == '__main__':
+    from functools import partial
+    from src.models.transformer import LitTransformer
+    from src.datamodules.belyaev_kushnarev import BelyaevKushnarevDataModule
+
+    model_class = LitTransformer
+    features = [
+        'm1vel',
+        'm2vel',
+        'm3vel',
+        'm1cur',
+        'm2cur',
+        'm3cur',
+        'Ke',
+    ]
+
+    datamodule_class = partial(
+        BelyaevKushnarevDataModule,
+        features=features,
+        mode='cls+reg',
+        segment_size=100,
+        batch_size=1000,
+        num_workers=2,
+    )
+
+
+    def suggest_params(trial: optuna.trial.Trial) -> dict[str, Any]:
+        sequence_length = trial.suggest_int('sequence_length', low=5, high=90, log=True)
+
+        n_in_layers = trial.suggest_int('n_in_layers', low=1, high=5, log=True)
+        in_layers = [trial.suggest_int(f'in_layer_{i}', low=8, high=256, log=True) for i in range(n_in_layers)]
+
+        n_out_reg_layers = trial.suggest_int('n_out_reg_layers', low=1, high=5, log=True)
+        out_reg_layers = [trial.suggest_int(f'out_reg_layer_{i}', low=8, high=256, log=True) for i in
+                          range(n_out_reg_layers)]
+
+        d_model = trial.suggest_int('d_model', low=16, high=256, log=True)
+        dim_feedforward = trial.suggest_int('dim_feedforward', low=16, high=512, log=True)
+
+        hparams = {
+            'input_dim':               len(features),
+            'sequence_length':         sequence_length,
+            'in_mlp_hidden_dims':      in_layers,
+            'out_reg_mlp_hidden_dims': out_reg_layers,
+            'd_model':                 d_model,
+            'dim_feedforward':         dim_feedforward,
+        }
+
+        return hparams
+
+
+    optimize(
+        model_class=model_class,
+        datamodule_class=datamodule_class,
+        suggest_hparams=suggest_params
+    )
