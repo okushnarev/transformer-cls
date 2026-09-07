cb8ffcd3f810c53a4f609b070d6e62710b96711f
okushnarev <styx.inc4@gmail.com>
2026-09-07 19:14:19 +0700

    Add random choice surf update strategy

--- src/surf_update_strategies.py
+++ src/surf_update_strategies.py
@@ -13,3 +13,7 @@ def min_diff_surf_upd(old_models: torch.Tensor, new_models: torch.Tensor) -> tor
     distances = torch.linalg.vector_norm(dist_of_models.flatten(start_dim=1), dim=-1)
     max_dist_idx = torch.argmin(distances)
     return new_models[max_dist_idx]
+
+def random_surf_upd(old_models: torch.Tensor, new_models: torch.Tensor) -> torch.Tensor:
+    rand_idx = torch.randint(high=new_models.shape[0], size=(1,))
+    return new_models[rand_idx]
