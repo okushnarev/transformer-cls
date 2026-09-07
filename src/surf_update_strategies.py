d0ad4783631b1b9dd6d37037498cf2ecbaedc260
okushnarev <styx.inc4@gmail.com>
2026-09-07 16:35:20 +0700

    Add max difference surf update strategy

---
+++ src/surf_update_strategies.py
@@ -0,0 +1,8 @@
+import torch
+
+
+def max_diff_surf_upd(old_models: torch.Tensor, new_models: torch.Tensor) -> torch.Tensor:
+    dist_of_models = new_models - old_models
+    distances = torch.linalg.vector_norm(dist_of_models.flatten(start_dim=1), dim=-1)
+    max_dist_idx = torch.argmax(distances)
+    return new_models[max_dist_idx]
