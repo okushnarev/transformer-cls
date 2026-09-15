dd84c4b34769fdee243a848ba0accd87210c0506
okushnarev <styx.inc4@gmail.com>
2026-09-15 20:18:53 +0700

    Add base BorealTCDataModule

---
+++ src/datamodules/borealtc.py
@@ -0,0 +1,47 @@
+from typing import Literal, Optional
+
+import pandas as pd
+import torch
+from pandas import DataFrame
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import TensorDataset
+
+from src.data_processing import create_sequences
+from src.datamodules.base import BaseDataModule
+
+
+class BorealTCDataModule(BaseDataModule):
+    def __init__(
+            self,
+            features: list[str],
+            mode: Literal['cls', 'reg', 'cls+reg'],
+            info_cols: list[str] | None,
+            reg_targets: list[str] | None,
+            segment_size: int,
+            sequence_length: int,
+            test_size: float,
+            val_size: float,
+            batch_size: int,
+            num_workers: int,
+            pin_memory: bool,
+            seed: int,
+    ):
+        super().__init__(
+            # Dataset-specific arguments
+            ds_name='boreal_imu.csv',
+            group_cols='exp_idx',
+            stratify_col='surf',
+            info_cols=info_cols,
+            cls_target='surf' if 'cls' in mode else None,
+            reg_targets=reg_targets if 'reg' in mode and reg_targets else [],
+            # General arguments
+            features=features,
+            segment_size=segment_size,
+            sequence_length=sequence_length,
+            test_size=test_size,
+            val_size=val_size,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            pin_memory=pin_memory,
+            seed=seed,
+        )
