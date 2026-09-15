6227f2b5fbbaaa9cc113cae19b4ccce3d1b29214
okushnarev <styx.inc4@gmail.com>
2026-09-15 10:59:07 +0700

    Remove import for non-existent class

--- src/models/verbose_transformer.py
+++ src/models/verbose_transformer.py
@@ -6,7 +6,7 @@ from torch import Tensor
 from torch.nn.functional import mse_loss
 
 from src.loss import div_attn_loss, sharp_attn_loss
-from src.models.base_model import LitMixedLossModel, LitRegressionModel, LitRegressionSelfAttnLoss
+from src.models.base_model import LitMixedLossModel, LitRegressionModel
 from src.models.modules import VerboseTransformerDecoder, VerboseTransformerDecoderLayer
 from src.models.transformer import Transformer
 from src.models.utils import LossTerm, VerboseModelOutputDecoder, init_weights
